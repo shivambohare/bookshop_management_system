702b5b25e7f81c50a33dff23dbd6630edec34f5d
Shivam Bohare <shivam.bohare.g@equifi.com>
2021-02-27 22:39:47 +0530

    Initial commit

---
+++ bookshop_management_system/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = env("DEBUG")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
+}
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
